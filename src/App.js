2633d4d6b22917c26d5197e7d71e60e73125d400
arierie <arieridwansyah@gmail.com>
2022-03-03 17:24:10 +0700

    Make social handlers always open a new tab

--- src/App.js
+++ src/App.js
@@ -8,12 +8,12 @@ function App() {
         <div className="Title">ordonnarie</div>
         <div className="Subtitle">A solo creative computing exploration project coming up into a web3 ecosystem. Music, generative art, illustration, and any other possibilities are up. Stay tune and wait for the next update.</div>
         <div className="Socials">
-          <a href="https://github.com/ordonnarie">Github</a>
-          <a href="https://twitter.com/ordonnarie">Twitter</a>
-          <a href="https://instagram.com/ordonnarie">Instagram</a>
-          <a href="https://audius.co/ordonnarie">Audius</a>
-          <a href="https://soundcloud.com/ordonnarie">Soundcloud</a>
-          <a href="https://www.tiktok.com/@ordonnarie">Tiktok</a>
+          <a href="https://github.com/ordonnarie" target="_blank" rel="noopener noreferrer">Github</a>
+          <a href="https://twitter.com/ordonnarie" target="_blank" rel="noopener noreferrer">Twitter</a>
+          <a href="https://instagram.com/ordonnarie" target="_blank" rel="noopener noreferrer">Instagram</a>
+          <a href="https://audius.co/ordonnarie" target="_blank" rel="noopener noreferrer">Audius</a>
+          <a href="https://soundcloud.com/ordonnarie" target="_blank" rel="noopener noreferrer">Soundcloud</a>
+          <a href="https://www.tiktok.com/@ordonnarie" target="_blank" rel="noopener noreferrer">Tiktok</a>
         </div>
     </div>
   );
